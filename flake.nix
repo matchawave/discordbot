e05c639fab47f57aa93a0e991f886457accb9507
gilbert <giloe.dev@gmail.com>
2025-09-23 13:17:40 -0600

    working with nix

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    naersk.url = "github:nix-community/naersk";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  };
+
+  outputs = { self, nixpkgs, naersk, flake-utils, nix-vscode-extensions }: 
+  flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ nix-vscode-extensions.overlays.default ];
+      config.allowUnfree = true;
+    };
+    naerskLib = pkgs.callPackage naersk {};
+    rustPackage = naerskLib.buildPackage {
+      src = ./.;
+      buildInputs = with pkgs; [ ];
+      nativeBuildInputs = with pkgs; [ ];
+    };
+  in {
+    packages.default = rustPackage;
+    devShells.default = pkgs.mkShell {
+      inputsFrom = [ rustPackage ];
+      nativeBuildInputs = with pkgs; [ 
+        rustc
+        cargo
+        rustfmt
+        rust-analyzer
+        # VSCode with extensions
+        (pkgs.vscode-with-extensions.override {
+          vscodeExtensions = with pkgs.vscode-marketplace; [
+            rust-lang.rust-analyzer
+          ];
+        })
+      ];
+    };
+  });
+}
